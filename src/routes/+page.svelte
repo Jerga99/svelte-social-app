80ee75efb70cb0a425efafd9c7f3d1ef1b4b31bf
Filip Jerga <jerga99@gmail.com>
2023-02-09 08:42:04 +1100

    Src Folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  console.log("Hello World!");
+</script>
+
+<div class="test">Hello World</div>
+
+<style>
+  .test {
+    color:blue;
+  }
+</style>
