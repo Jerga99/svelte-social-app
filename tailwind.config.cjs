49a329e87cb780e56801cec19660fec177f991bd
Filip Jerga <jerga99@gmail.com>
2023-02-09 09:22:47 +1100

    Additional CSS Improvements

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,32 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const generateSizeClass = (upToSize, startAt = 80) => {
+  const classes  = {};
+  for (let i = startAt; i < upToSize / 4; i+= 4 ) {
+    classes[i] = `${(i * 4) / 16}rem`;
+  }
+
+  return classes;
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      "xxs": "300px",
+      "xs": "475px",
+      ...defaultTheme.screens
+    },
+    extend: {
+      width: generateSizeClass(1024),
+      minHeight: generateSizeClass(1024, 0),
+      maxHeight: generateSizeClass(1024, 0),
+      maxWidth: generateSizeClass(1024, 0),
+      minWidth: generateSizeClass(1024, 0),
+      borderWidth: {
+        "1": "1px"
+      }
+    }
+  }
+};
