7aea00c6fbb11812f366de90d0d750822986abd0
Filip Jerga <jerga99@gmail.com>
2023-02-10 07:44:18 +1100

    More about layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,20 @@
   import "../app.css";
 </script>
 
-<slot />
+<div class="text-white p-20 bg-yellow-500">
+  I am root layout
+  <slot />
+</div>
+
+-------------
+-------------
+
+<div class="text-white p-20 bg-yellow-500">
+  I am root layout
+  <div class="text-white p-4 bg-blue-500">
+    I am task manager layout
+    <div class="text-white">
+      I am a task manager page
+    </div>
+  </div>
+</div>
---
+++ src/routes/task-manager/+layout.svelte
@@ -0,0 +1,5 @@
+
+<div class="text-white p-4 bg-blue-500">
+  I am task manager layout
+  <slot />
+</div>
