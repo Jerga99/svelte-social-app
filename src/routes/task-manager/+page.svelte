886204c7371c3512b686d0ac0927827e75e038a1
Filip Jerga <jerga99@gmail.com>
2023-02-10 08:09:30 +1100

    Reactive statement

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,14 +1,21 @@
-
 <script>
-  let count = 0;
+	let count = 0;
+
+	$: {
+		if (count > 10) {
+			console.log('I am the winner!');
+		}
+	}
 </script>
 
 <div class="text-white">
-  <div>I am a task manager page - {count}</div>
+	<div>I am a task manager page - {count}</div>
 
-  <button on:click={() => {
-    count++;
-  }}>
-    Increment
-  </button>
+	<button
+		on:click={() => {
+			count++;
+		}}
+	>
+		Increment
+	</button>
 </div>
