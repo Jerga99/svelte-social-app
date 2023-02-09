ae02c9d188565ab178ef9b3c8b3a707466867383
Filip Jerga <jerga99@gmail.com>
2023-02-10 08:35:01 +1100

    Task Item

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import TaskItem from "../../components/task-manager/TaskItem.svelte";
+</script>
+
 <div class="p-10 h-full">
 	<div class="text-white text-2xl mb-6">Some List</div>
 	<button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
@@ -32,33 +36,12 @@
 						</div>
 					</div>
 					<div class="overflow-x-hidden overflow-y-auto with-scrollbar p-2">
-						<!-- TASK ITEM START -->
-						<div class="flex-it border border-solid p-2 rounded-xl bg-slate-500 mb-2 cursor-pointer">
-							<div class="flex-it">
-								<div class="flex-it flex-row">
-									<div class="flex flex-1">Some Task</div>
-									<div class="flex items-end hover:text-red-600">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width="16"
-											height="16"
-											fill="currentColor"
-											class="bi bi-trash"
-											viewBox="0 0 16 16"
-										>
-											<path
-												d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-											/>
-											<path
-												fill-rule="evenodd"
-												d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-											/>
-										</svg>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- TASK ITEM END -->
+						<TaskItem />
+						<TaskItem />
+						<TaskItem />
+						<TaskItem />
+						<TaskItem />
+						<TaskItem />
 					</div>
 					<button class="underline flex p-2"> + Add Task </button>
 				</div>
