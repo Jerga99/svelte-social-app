a17dc5c7285fb70d6f25659919e560185a4a1831
Filip Jerga <jerga99@gmail.com>
2023-02-10 08:16:14 +1100

    Reactive declarations

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -6,11 +6,14 @@
 			console.log('I am the winner!');
 		}
 	}
+
+  $: doubled = count * 2;
+  
 </script>
 
 <div class="text-white">
 	<div>I am a task manager page - {count}</div>
-
+  <div>Doubled value: {doubled}</div>
 	<button
 		on:click={() => {
 			count++;
