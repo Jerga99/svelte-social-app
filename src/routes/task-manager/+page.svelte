4b83d58fa53c8e9d9f2c22df0f07933c806da591
Filip Jerga <jerga99@gmail.com>
2023-02-15 08:53:06 +1100

    Task List Data

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -2,7 +2,17 @@
   import TaskList from "../../components/task-manager/TaskList.svelte";
 
   const listName = "Hi There!";
+  const taskList = [
+    {id: "l-1", text: "List 1", items: [{id: "t-1", text: "Task 1"},{id: "t-2", text: "Task 2"},{id: "t-3", text: "Task 3"}]},
+    {id: "l-2", text: "List 2", items: [{id: "t-4", text: "Task 4"},{id: "t-5", text: "Task 5"},{id: "t-6", text: "Task 6"}]},
+    {id: "l-3", text: "List 3", items: [{id: "t-7", text: "Task 7"},{id: "t-8", text: "Task 8"},{id: "t-9", text: "Task 9"}]},
+  ];
+
 </script>
+
+<div class="text-white">
+  {JSON.stringify(taskList)}
+</div>
 
 <div class="p-10 h-full">
   <div class="text-white text-2xl mb-6">Some List</div>
