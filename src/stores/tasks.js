56bbc95491198adaf25e278e49b7e751c09398b0
Filip Jerga <jerga99@gmail.com>
2023-02-16 09:45:30 +1100

    Start Function

--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -2,13 +2,16 @@
 import { writable } from "svelte/store";
 
 function createStore() {
-  const taskList = writable([]);
-  let i = 0;
 
-  setInterval(() => {
-    taskList.update((list) => [...list, i++])
-  }, 2000);
+  const taskList = writable([], () => {
+    let i = 0;
+    console.log("Subscribed to the store!");
 
+    setInterval(() => {
+      taskList.update((list) => [...list, i++])
+    }, 2000);
+  });
+  
   return taskList;
 }
 
