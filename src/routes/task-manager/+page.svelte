d19c1d499a085bcdc9875e38e50aa1994e819f0a
Filip Jerga <jerga99@gmail.com>
2023-02-17 07:22:31 +1100

    Task List Store Data

--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,31 +1,16 @@
 <script>
   import TaskList from "../../components/task-manager/TaskList.svelte";
   import { taskListStore } from "../../stores/tasks";
-
-  const taskList = [
-    {id: "l-1", text: "List 1", items: [{id: "t-1", text: "Task 1"},{id: "t-2", text: "Task 2"},{id: "t-3", text: "Task 3"}]},
-    {id: "l-2", text: "List 2", items: [{id: "t-4", text: "Task 4"},{id: "t-5", text: "Task 5"},{id: "t-6", text: "Task 6"}]},
-    {id: "l-3", text: "List 3", items: [{id: "t-7", text: "Task 7"},{id: "t-8", text: "Task 8"},{id: "t-9", text: "Task 9"}]},
-  ];
 </script>
 
 <div class="p-10 h-full">
-
-  <a href="/" class="text-white">
-    GO HOME
-  </a>
-
-  <div class="text-white">
-    {JSON.stringify($taskListStore)}
-  </div>
-
   <div class="text-white text-2xl mb-6">Some List</div>
   <button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
     + Add List
   </button>
   <div class="flex-it h-full">
     <div class="flex-it flex-row rounded-xl h-full">
-      {#each taskList as list (list.id)}
+      {#each $taskListStore as list (list.id)}
         <TaskList 
           listName={list.text} 
           tasks={list.items}
--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -1,21 +1,14 @@
 
 import { writable } from "svelte/store";
 
+const DEFAUL_DATA = [
+  {id: "l-1", text: "List 1", items: [{id: "t-1", text: "Task 1"},{id: "t-2", text: "Task 2"},{id: "t-3", text: "Task 3"}]},
+  {id: "l-2", text: "List 2", items: [{id: "t-4", text: "Task 4"},{id: "t-5", text: "Task 5"},{id: "t-6", text: "Task 6"}]},
+  {id: "l-3", text: "List 3", items: [{id: "t-7", text: "Task 7"},{id: "t-8", text: "Task 8"},{id: "t-9", text: "Task 9"}]},
+];
+
 function createStore() {
-
-  const taskList = writable([], () => {
-    let i = 0;
-   
-    const id = setInterval(() => {
-      console.log("Updating Value");
-      taskList.update((list) => [...list, i++])
-    }, 2000);
-
-    return function stop() {
-      clearInterval(id);
-    }
-  });
-  
+  const taskList = writable(DEFAUL_DATA);
   return taskList;
 }
 
