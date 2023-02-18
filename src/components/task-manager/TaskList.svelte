13ff48bafa44bd69b8429253df5809940633d5ff
Filip Jerga <jerga99@gmail.com>
2023-02-19 09:25:55 +1100

    Hovering Effect

--- src/components/task-manager/TaskList.svelte
+++ src/components/task-manager/TaskList.svelte
@@ -25,6 +25,7 @@
     }}
     on:dragover|preventDefault={()=>{}}
     on:drop={drop}
+    class:hovering={list.id === listHoverId}
     class="bg-slate-400 flex-it rounded-xl max-h-full border-2 border-gray-500"
   >
     <div class="flex-it m-3">
@@ -65,3 +66,10 @@
     </button>
   </div>
 </div>
+
+
+<style>
+  .hovering {
+    border: 2px solid orange;
+  }
+</style>
