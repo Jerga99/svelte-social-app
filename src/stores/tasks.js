5b85de075412ef3026d679ade35d1c32161956aa
Filip Jerga <jerga99@gmail.com>
2023-02-20 09:46:38 +1100

    Remove item function

--- src/stores/tasks.js
+++ src/stores/tasks.js
@@ -55,6 +55,9 @@ function createStore() {
         list[moveToListIdx].items.push(task);
         return list;
       })
+    },
+    removeTask: () => {
+      alert("Removing task!");
     }
   };
 }
