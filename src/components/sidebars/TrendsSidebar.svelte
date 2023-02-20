f79ea2d6bdb3f4c0cf7044a13b5af63d4265cc44
Filip Jerga <jerga99@gmail.com>
2023-02-21 08:42:56 +1100

    Trends data

--- src/components/sidebars/TrendsSidebar.svelte
+++ src/components/sidebars/TrendsSidebar.svelte
@@ -1,3 +1,45 @@
+
+<script>
+
+  const randomize = () => Math.floor(Math.random() * 500);
+
+  const trends = [
+    {
+      category: "Sports",
+      content: "Some team won something!",
+      glideCount: randomize()
+    },
+    {
+      category: "Finance",
+      content: "Bitcoin down again!",
+      glideCount: randomize()
+    },
+    {
+      category: "PC & Games",
+      content: "New Eincode game out!",
+      glideCount: randomize()
+    },
+    {
+      category: "Economy",
+      content: "It's goin well...",
+      glideCount: randomize()
+    },
+    {
+      category: "Celebrities",
+      content: "Some useless news!",
+      glideCount: randomize()
+    },
+    {
+      category: "Movies",
+      content: "Peter Jackson as the director of new Lotr",
+      glideCount: randomize()
+    },
+  ]
+
+  console.log(trends);
+</script>
+
+
 <div class="bg-gray-800 overflow-hidden flex-it rounded-2xl">
   <div class="flex-it p-4">
     <span class="text-xl font-bold">Trends</span>
