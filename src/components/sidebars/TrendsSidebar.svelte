6f2c994327a10f437170cb10e689e979ebe630f5
Filip Jerga <jerga99@gmail.com>
2023-02-21 08:48:05 +1100

    Each Loop Trend

--- src/components/sidebars/TrendsSidebar.svelte
+++ src/components/sidebars/TrendsSidebar.svelte
@@ -44,46 +44,13 @@
   <div class="flex-it p-4">
     <span class="text-xl font-bold">Trends</span>
   </div>
-  <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-    <div class="flex-it">
-      <span class="text-gray-400 text-sm"> Trends in Europe </span>
-      <span class="text-lg font-bold">Sports</span>
-      <span class="text-gray-400 text-sm">10 000 glides</span>
+  {#each trends as trend }
+    <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
+      <div class="flex-it">
+        <span class="text-gray-400 text-sm">{trend.content}</span>
+        <span class="text-lg font-bold">{trend.category}</span>
+        <span class="text-gray-400 text-sm">{trend.glideCount} glides</span>
+      </div>
     </div>
-  </div>
-  <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-    <div class="flex-it">
-      <span class="text-gray-400 text-sm"> Trends in Europe </span>
-      <span class="text-lg font-bold">Sports</span>
-      <span class="text-gray-400 text-sm">10 000 glides</span>
-    </div>
-  </div>
-  <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-    <div class="flex-it">
-      <span class="text-gray-400 text-sm"> Trends in Europe </span>
-      <span class="text-lg font-bold">Sports</span>
-      <span class="text-gray-400 text-sm">10 000 glides</span>
-    </div>
-  </div>
-  <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-    <div class="flex-it">
-      <span class="text-gray-400 text-sm"> Trends in Europe </span>
-      <span class="text-lg font-bold">Sports</span>
-      <span class="text-gray-400 text-sm">10 000 glides</span>
-    </div>
-  </div>
-  <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-    <div class="flex-it">
-      <span class="text-gray-400 text-sm"> Trends in Europe </span>
-      <span class="text-lg font-bold">Sports</span>
-      <span class="text-gray-400 text-sm">10 000 glides</span>
-    </div>
-  </div>
-  <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-    <div class="flex-it">
-      <span class="text-gray-400 text-sm"> Trends in Europe </span>
-      <span class="text-lg font-bold">Sports</span>
-      <span class="text-gray-400 text-sm">10 000 glides</span>
-    </div>
-  </div>
+  {/each}
 </div>
