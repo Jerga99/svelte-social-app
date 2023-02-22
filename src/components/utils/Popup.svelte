85238ea27f8d012c6efcf48cbde470117de85f47
Filip Jerga <jerga99@gmail.com>
2023-02-23 08:23:50 +1100

    Logout Menu

--- src/components/utils/Popup.svelte
+++ src/components/utils/Popup.svelte
@@ -3,15 +3,19 @@
   let isOpen = false;
 </script>
 
-<div class="relative">
+<div class="flex-it">
   <div class="flex-it">
     <button on:click={() => isOpen = true}>
       <slot />
     </button>
   </div>
   {#if isOpen}
-    <div class="flex-it w-20 h-20 fixed bg-black bottom-10 popup">
-      Hello World
+    <div class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000">
+      <div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
+        <div class="flex-it flex-grow flex-shrink py-3">
+          <div class="flex-it px-4 py-3 transition hover:bg-gray-700">Logout</div>
+        </div>
+      </div>
     </div>
   {/if}
 </div>
