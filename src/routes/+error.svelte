196619f049992fb16e27d3016c95e402687c52d6
Filip Jerga <jerga99@gmail.com>
2023-02-22 11:19:03 +1100

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div class="flex-it justify-center items-center h-full text-white">
+  <div class="text-center">
+    <div class="text-4xl font-bold">
+      {$page.status} - {$page.error.message}
+    </div>
+    <a href="/" class="underline block text-center">Go to home page</a>
+  </div>
+</div>
