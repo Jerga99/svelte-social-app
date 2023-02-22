4e9553fbddcb5d4b62402c0aa9f5c4e9b4bcc330
Filip Jerga <jerga99@gmail.com>
2023-02-23 09:29:26 +1100

    Portal

--- src/components/utils/Popup.svelte
+++ src/components/utils/Popup.svelte
@@ -1,6 +1,7 @@
 
 <script>
   import { beforeUpdate, afterUpdate, onMount, onDestroy } from "svelte";
+  import Portal from "./Portal.svelte";
 
   let isOpen = false;
   let openerMenu;
@@ -12,10 +13,6 @@
     return () => {
       removeEventListener("click", closePopup);
     }
-  })
-
-  beforeUpdate(() => {
-    console.log("before update called!");
   })
 
   afterUpdate(() => {
@@ -38,15 +35,17 @@
     </button>
   </div>
   {#if isOpen}
-    <div 
-      style="bottom: {popupBottomPosition}"
-      class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000"
-    >
-      <div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
-        <div class="flex-it flex-grow flex-shrink py-3">
-          <div class="flex-it px-4 py-3 transition hover:bg-gray-700">Logout</div>
+    <Portal>
+      <div 
+        style="bottom: {popupBottomPosition}"
+        class="flex-it hover:cursor-pointer fixed bg-gray-800 text-white popup z-10 rounded-2xl border-gray-700 border transition duration-1000"
+      >
+        <div class="w-72 min-w-68 max-h-120 min-h-8 flex-it overflow-auto">
+          <div class="flex-it flex-grow flex-shrink py-3">
+            <div class="flex-it px-4 py-3 transition hover:bg-gray-700">Logout</div>
+          </div>
         </div>
       </div>
-    </div>
+    </Portal>
   {/if}
 </div>
