6a928ebaf4dbf2bfc69ac4f70d5b01f62135e59e
Filip Jerga <jerga99@gmail.com>
2023-02-25 09:09:27 +1000

    Simulate Authentication

--- src/components/context/auth/AuthContext.svelte
+++ src/components/context/auth/AuthContext.svelte
@@ -4,8 +4,13 @@
   import { writable } from "svelte/store";
   import { key } from ".";
 
-  let isAuthenticated = writable(false);
-  let isLoading = writable(false);
+  let isLoading = writable(true);
+  let isAuthenticated = writable(false, (set) => {
+    setTimeout(() => {
+      set(true); // isAuthenticated setter
+      isLoading.set(false);
+    }, 2000);
+  });
 
   setContext(key, {
     isAuthenticated, isLoading
