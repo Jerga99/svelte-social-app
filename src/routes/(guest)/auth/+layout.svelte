e3cd41720969a0447e6a5e80ae3e76f3db8ffb14
Filip Jerga <jerga99@gmail.com>
2023-02-25 18:55:47 +1000

    Redirect if authenticated

---
+++ src/routes/(guest)/auth/+layout.svelte
@@ -0,0 +1,17 @@
+
+<script>
+  import { goto } from "$app/navigation";
+  import { getAuthContext } from "@components/context/auth";
+
+  const { isAuthenticated } = getAuthContext();
+
+  if ($isAuthenticated) {
+    goto("/");
+  }
+
+</script>
+
+{#if !isAuthenticated}
+  <slot />
+{/if}
+  
