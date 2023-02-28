bdcaca5a2182011299a5023d1d53d61288e15585
Filip Jerga <jerga99@gmail.com>
2023-02-28 11:14:43 +1000

    Pass errors as props

--- src/components/forms/FormErrors.svelte
+++ src/components/forms/FormErrors.svelte
@@ -1,7 +1,12 @@
 
+<script>
+  export let errors;
+</script>
 
-
-
-<div class="flex-it grow text-xs bg-red-400 text-white p-3 pl-3 mt-1 rounded-md">
-  <slot />
-</div>
+{#if errors?.length > 0}
+  <div class="flex-it grow text-xs bg-red-400 text-white p-3 pl-3 mt-1 rounded-md">
+    {#each errors as error}
+      <div>{error}</div>
+    {/each}
+  </div>
+{/if}
--- src/components/forms/RegisterForm.svelte
+++ src/components/forms/RegisterForm.svelte
@@ -32,9 +32,7 @@
             id="fullName"
             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
           />
-          <FormErrors>
-            {JSON.stringify($errors.fullName)}
-          </FormErrors>
+          <FormErrors errors={$errors.fullName} />
         </div>
 
         <div class="flex-it py-2">
@@ -47,9 +45,7 @@
             id="nickName"
             class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
           />
-          <FormErrors>
-            {JSON.stringify($errors.nickName)}
-          </FormErrors>
+          <FormErrors errors={$errors.nickName} />
         </div>
 
         <div class="flex-it py-2">
