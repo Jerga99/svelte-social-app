c4a9d1f274662420741202da3df09447cb3a8630
Filip Jerga <jerga99@gmail.com>
2023-03-03 08:34:40 +1000

    Add treshold to load glides

--- src/components/glides/PaginatedGlides.svelte
+++ src/components/glides/PaginatedGlides.svelte
@@ -3,11 +3,24 @@
   import GlidePost from "./GlidePost.svelte";
 
   export let pages;
+
+  let lastItemRef;
+
+  function loadNewGlides() {
+    if (lastItemRef.getBoundingClientRect().top <= window.innerHeight) {
+      console.log("Load more glides!");
+    }
+  }
+
 </script>
 
+<svelte:window on:scroll={loadNewGlides} />
 
 {#each Object.keys(pages) as page }
   {#each pages[page].glides as glide (glide.id) }
       <GlidePost {glide}/>
   {/each}
 {/each}
+
+<div bind:this={lastItemRef}></div>
+<div class="h-96"></div>
