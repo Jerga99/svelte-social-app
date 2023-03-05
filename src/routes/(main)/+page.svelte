6c9b8f3c61b734bf088919d4885e12a268639917
Filip Jerga <jerga99@gmail.com>
2023-03-05 19:59:51 +1000

    Button to display more glides

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -2,6 +2,7 @@
   import { getAuthContext } from "@components/context/auth";
   import PaginatedGlides from "@components/glides/PaginatedGlides.svelte";
   import Messenger from "@components/utils/Messenger.svelte";
+  import Portal from "@components/utils/Portal.svelte";
   import { createGlideStore } from "@stores/createGlideStore";
   import { pageStore } from "@stores/pageStore";
   import { onMount } from "svelte";
@@ -13,16 +14,29 @@
 
   onMount(subscribeToNewGlides);
 
-  $: {
-    console.log($freshGlides);
-  }
-  
 </script>
 
 <Messenger 
   onGlidePosted={addGlide}
 />
 <div class="h-px bg-gray-700 my-1" />
+
+{#if $freshGlides.length >= 3}
+  <Portal>
+    <div class="fixed top-2 z-100 left-2/4 -translate-x-1/2">
+      <button
+        type="button"
+        class="
+				disabled:cursor-not-allowed disabled:bg-gray-400
+				bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition duration-200"
+      >
+        <div class="flex-it flex-row text-sm font-bold text-white items-start justify-center">
+          <span>Read New Glides</span>
+        </div>
+      </button>
+    </div>
+  </Portal>
+{/if}
 
 <PaginatedGlides 
   pages={$pages} 
